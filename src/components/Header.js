38b34b54cd84180e9fe6e94b9d491ef5df8aaa83
John Otander <johnotander@gmail.com>
2016-08-19 13:21:14 -0600

    Add bg gradient

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default () => (
-  <div className='bb bw2 code pa2 f6 b'>
+  <div className='bb bw2 code pa2 f6 b bg-white'>
     ⊡
     <a href='#' className='ml3 black link'>File</a>
     <a href='#' className='ml2 black link'>Edit</a>
--- src/components/Window.js
+++ src/components/Window.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
 export default ({ children }) => (
-  <div className='ba code'>
+  <div className='ba code bg-white'>
     <div className='bb br bw2' children={children} />
   </div>
 )
 
 export const WindowHeader = ({ children }) => (
-  <div className='bb' children={children} />
+  <div className='tc bb relative'>
+    <div className='pa2 bg-white z-3'>
+      {children}
+    </div>
+  </div>
 )
