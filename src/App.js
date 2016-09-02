c85e32b3b44fbbfc4d90b1da05a1faa10f860bc7
John Otander <johnotander@gmail.com>
2016-09-02 14:39:12 -0700

    Begin working on docs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 
+import style from '../style'
 import Desktop from './components/Desktop'
 import Header from './components/Header'
 import Window, { WindowHeader } from './components/Window'
@@ -10,45 +11,42 @@ import Input from './components/Input'
 
 class App extends Component {
   render () {
+    const { base: { fontFamily } } = style
+
     return (
-      <Desktop style={{minHeight: '100vw'}}>
-        <Header />
-        <div className='mt4 ml3 w-60'>
-          <Window>
-            <WindowHeader children='Tosh' />
-            <div className='bb pa2'>
-              January 19, 1983
-            </div>
-            <p className='measure pa2'>
+      <div style={{fontFamily}} className='ph2 pb3 pb4-m pb6-l'>
+        <div className='bb bw2'>
+          <div className='measure-wide center pv3 pv4-m pv5-l'>
+            <h1 className='f-subheadline-ns'>Tosh</h1>
+            <p className='lh-copy measure mid-gray pb2 pb4-m pb4-l'>
               The Lisa is a personal computer introduced by Apple Computer Inc. on January 19, 1983.
-              It was one of the first personal computers to offer a graphical user interface (GUI) in a machine aimed at individual business users.
+              It was one of the first personal computers to offer a graphical user interface (GUI) in a machine aimed at individual business users. <br /><br />
+              <a href='https://github.com/johnotander/tosh' className='black link'>Tosh</a> is a UI library that travels back to those humble beginnings.
+            </p>
+          </div>
+        </div>
+        <div className='measure-wide center'>
+          <h4>Desktop</h4>
+          <p className='lh-copy measure mid-gray'>
+            The Desktop component provides a retro background.
+            It uses a css gradient so it is lightweight.
+          </p>
+          <pre>&lt;Desktop&gt;...&lt;/Desktop&gt;</pre>
+          <Desktop className='w-100 h5' />
+          <h4>Window</h4>
+          <p className='lh-copy measure mid-gray'>
+            The Window component can be used to separate content.
+            It looks great when placed within a Desktop component.
+          </p>
+          <pre>&lt;Window&gt;&lt;WindowHeader&gt;...&lt;/WindowHeader&gt;...&lt;/Window&gt;</pre>
+          <Window className='w-100'>
+            <WindowHeader children='Window' />
+            <p className='lh-copy measure-narrow mid-gray ph2'>
+              Wrap content into logical parts with the Window component.
             </p>
           </Window>
         </div>
-        <div className='mt3 center w-50'>
-          <Window>
-            <WindowHeader children='About Tosh' />
-            <div className='bb pa2'></div>
-            <div className='pa2'>
-              <div className='dt mb2'>
-                <div className='dtc v-mid' children='Pct Awesome (87%)' />
-                <div className='dtc v-mid w4 pl2'>
-                  <Progress value={.87} />
-                </div>
-              </div>
-              <div className='bt mt2 pt2'>
-                <Input placeholder='An input' />
-                <div className='pt2'>
-                  <Textarea placeholder='A textarea' />
-                </div>
-                <div className='pt2'>
-                  <Button children='Awesome' />
-                </div>
-              </div>
-            </div>
-          </Window>
-        </div>
-      </Desktop>
+      </div>
     )
   }
 }
